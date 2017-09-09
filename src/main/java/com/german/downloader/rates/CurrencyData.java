d0b0df4fc06c26cda55b93dcff80062216bc9ac4
jaros <judin.jaroslav@gmail.com>
2017-09-10 00:27:18 +0200

    extract exchange rates supplier

---
+++ src/main/java/com/german/downloader/rates/CurrencyData.java
@@ -0,0 +1,48 @@
+package com.german.downloader.rates;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CurrencyData {
+
+    private static final Logger logger = LoggerFactory.getLogger(CurrencyData.class);
+    private static final String forDateUrl = "http://api.fixer.io/%s?base=%s";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private Map<LocalDate, List<ExchangeRate>> ratesCache = new HashMap<>();
+    private final List<String> fetchedCurrencies;
+
+
+    public CurrencyData(List<String> fetchedCurrencies) {
+        this.fetchedCurrencies = fetchedCurrencies;
+    }
+
+    public List<ExchangeRate> lastExchangeRates() {
+        return ratesCache.computeIfAbsent(LocalDate.now(), this::externalExchangeRates);
+    }
+
+    private List<ExchangeRate> externalExchangeRates(LocalDate forDate) {
+        return fetchedCurrencies.stream()
+                .map(currency -> exchangeRateForCurrency(currency, forDate))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private Optional<ExchangeRate> exchangeRateForCurrency(String currency, LocalDate date) {
+        try {
+            URL url = new URL(String.format(forDateUrl, date, currency));
+            return Optional.of(objectMapper.readValue(url, ExchangeRate.class));
+        } catch (IOException e) {
+            logger.error("cannot get exchange rate for {}", currency);
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/main/java/com/german/downloader/rates/RatesViewList.java
@@ -0,0 +1,23 @@
+package com.german.downloader.rates;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+import static java.util.Arrays.asList;
+
+
+@Controller
+@RequestMapping("/rates")
+public class RatesViewList {
+
+    private CurrencyData currencyData = new CurrencyData(asList("EUR", "USD", "CHF", "RUB"));
+
+    @GetMapping
+    ModelAndView rates() throws IOException {
+        return new ModelAndView("rates/list", "rates", currencyData.lastExchangeRates());
+    }
+}
